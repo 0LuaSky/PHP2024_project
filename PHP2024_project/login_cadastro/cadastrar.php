ee8cb684b7f472933f608be5b2d914d5f1b10d5b
Nyckolas Vives <150102502+Nyckolasvf@users.noreply.github.com>
2024-06-14 04:31:59 -0300

    Tiltei, vejo isso amanhã!
    Tentei terminar o sistema de login mas por algum motivo **novamente** ele não quer funcionar e Deus sabe a razão (ele não consegue entrar no if e vai direto para o else por conta do id).

---
+++ PHP2024_project/login_cadastro/cadastrar.php
@@ -0,0 +1,11 @@
+<!-- Formulário para o sistema de Cadastro e Login. -->
+<form method="POST" action="cadastrar_action.php">
+    <label>
+        Nome: <input type="text" name="nome"/>
+    </label>
+    <label>
+        Email: <input type="email" name="email"/>
+    </label>
+    <input type="submit" value="Salvar"/>
+</form>
+<a href="usuarios_cadastrados.php">Usuários cadastrados</a>
